dabaefec2c95c54d6ac4270f0a2e94ae9f7d42ac
Eduardo <edu.ice7@gmail.com>
2018-12-17 19:35:06 +0000

    Add files via upload

---
+++ data/config.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Pedro Arenas
+ * @author Franco Sanllehi <franco@fsanllehi.me>
+ *
+ * @license MIT
+ * Portuguese translation ice41
+ */
+
+$script_version = "V1.5";
+
+/* Configuracion de conexion query hacia TS3 */
+$HOST_QUERY = ""; //HOST:QUERY_PORT Que usa tu servidor de ts3
+$PORT_QUERY = "10101"; //Puerto de query por default es 10101
+$USER_QUERY = "serveradmin"; //Usuario de conexion a query
+$PASS_QUERY = ""; //Clave generada para a conexion query
+$SERVER_PORT = "9987"; //Puerto del servidor ts3
+
+/* Configuracion de variables Front-end */
+
+$NAME_TITLE = 'Nped TS3 icons'; //Titulo principal de la web
+$IDIOMA = "pt"; // (Options: en, es) Idioma de ejecucion
+$SID_GROUP = array(19,20,21,22,23,24,27,29,30,31,33,34,35,36,38,39,41,46,47,49,50,58); //SORT_ID del grupo que sera listado Example array(1,2,3,4,5)
+$NICK_QUERY = "NpedWebIcon"; //Nickname de la conexion query
+$MAX_ICONS = 5; //Maximo de iconos seleccionar
+$DEBUG = true; //Activa los mensajes de error detallados
+/* 16,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,38,39,41,46,47,50 */
